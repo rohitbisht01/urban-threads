acfaed70b62a09fdbf6a4f8bbf09d0a0d96cb842
rohitbisht01 <rb9519208@gmail.com>
2024-09-08 23:52:53 +0530

    fix: cookie changes

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -95,7 +95,7 @@ const loginUser = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
       })
       .json({
         success: true,
